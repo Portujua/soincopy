a2f74e45aa592274fd1022ccaaccf26bd5bc9044
Portujua <ejlorenzo19@gmail.com>
2016-07-27 04:08:50 -0400

    añadido filtro para solo los de estado==1

--- js/app-config.js
+++ js/app-config.js
@@ -1,3 +1,17 @@
 (function(){
 	var app = angular.module("soincopy", ["ngRoute", "angular.filter"]);
+
+	app.filter('quitarDeshabilitados', function () {
+	    return function (input) {
+	    	if (!input) return null;
+	    	
+	    	var out = [];
+
+	    	for (var i = 0; i < input.length; i++)
+	    		if (input[i].estado == 1)
+	    			out.push(input[i]);
+
+	    	return out;
+	    };
+	});
 }());
