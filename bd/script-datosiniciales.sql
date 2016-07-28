b845f39223e17a6690231f1f4a8ba43b444b1a96
Portujua <ejlorenzo19@gmail.com>
2016-07-28 04:07:50 -0400

    corregido permiso mal asignado

--- bd/script-datosiniciales.sql
+++ bd/script-datosiniciales.sql
@@ -61,7 +61,7 @@ insert into Permiso_Categoria (nombre) values ("Departamentos UCAB");
 insert into Permiso_Categoria (nombre) values ("Dependencias");
 
 insert into Permiso (nombre, descripcion, riesgo, categoria) values ("anadir_guias", "Podrá añadir nuevas guías al sistema", 4, 1);
-insert into Permiso (nombre, descripcion, riesgo, categoria) values ("buscar_guias", "Podrá consultar las guías disponibles en el sistema", 1, 2);
+insert into Permiso (nombre, descripcion, riesgo, categoria) values ("buscar_guias", "Podrá consultar las guías disponibles en el sistema", 2, 1);
 insert into Permiso (nombre, descripcion, riesgo, categoria) values ("modificar_guias", "Podrá modificar cualquier información de una guía disponible en el sistema", 6, 1);
 insert into Permiso (nombre, descripcion, riesgo, categoria) values ("anadir_orden", "Podrá generar ordenes", 4, 2);
 insert into Permiso (nombre, descripcion, riesgo, categoria) values ("personal_ver", "Podrá consultar el personal existente en el sistema", 0, 4);
