ae4769d5f9b2fa83754fa843cecada642eac2766
Portujua <ejlorenzo19@gmail.com>
2016-07-28 20:13:02 -0400

    corregido para que al editar no marque como repetido

--- js/directives/UniqueBD.js
+++ js/directives/UniqueBD.js
@@ -7,13 +7,16 @@
 					var val = ele[0].value;
 					var obj = attrs.uniqueBd;
 
-					$http.get("api/check/" + obj + "/" + val)
-						.success(function(data){
-							c.$setValidity('unique', data.esValido);
-						})
-						.error(function(data){
-							c.$setValidity('unique', false);
-						})
+					if (window.location.hash.indexOf("editar") == -1)
+						$http.get("api/check/" + obj + "/" + val)
+							.success(function(data){
+								c.$setValidity('unique', data.esValido);
+							})
+							.error(function(data){
+								c.$setValidity('unique', false);
+							})
+					else
+						c.$setValidity('unique', true);
 				})
 			}
 		}
