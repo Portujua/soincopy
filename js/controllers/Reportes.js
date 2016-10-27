1e598a61dea3be184fe04225963d99ab0d54eec5
Portujua <ejlorenzo19@gmail.com>
2016-10-27 04:05:19 -0400

    totalizados reportes

--- js/controllers/Reportes.js
+++ js/controllers/Reportes.js
@@ -47,6 +47,37 @@
 		$scope.seleccionar = function(o){
 			$scope.o_ = o;
 		}
+
+		$scope.totalizar = function(){
+			$scope.total = {
+				subtotal: 0.00,
+				iva: 0.00,
+				total: 0.00,
+
+				total_facturado: 0.00,
+				devoluciones: 0.00,
+				nota_de_credito: 0.00,
+				retiro_de_caja: 0.00
+			}
+
+			if (window.location.hash.indexOf('libro_de_ventas') != -1)
+				for (var i = 0; i < $scope.data.length; i++)
+				{
+					$scope.total.iva += parseFloat($scope.data[i].iva);
+					$scope.total.subtotal += parseFloat($scope.data[i].subtotal);
+					$scope.total.total += parseFloat($scope.data[i].total);
+				}
+			else if (window.location.hash.indexOf('cuadre_ventas_diarias') != -1)
+				for (var i = 0; i < $scope.data.length; i++)
+				{
+					$scope.total.total_facturado += parseFloat($scope.data[i].total_facturado);
+					$scope.total.devoluciones += parseFloat($scope.data[i].devoluciones);
+					$scope.total.nota_de_credito += parseFloat($scope.data[i].nota_de_credito);
+					$scope.total.retiro_de_caja += parseFloat($scope.data[i].retiro_de_caja);
+				}
+
+			$timeout($scope.totalizar, 1000);
+		}
 	};
 
 	angular.module("soincopy").controller("Reportes", Reportes);
